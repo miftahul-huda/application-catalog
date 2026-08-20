f64d9c1eba8c0e10cda73f847b1ab698f4e7bbcf
M. Huda <mhuda@IDL0034.local>
2026-08-20 12:50:45 +0700

    fix: Install curl in Dockerfile and read PORT env variable for Cloud Run deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const { sequelize } = require('./models');
 
 const app = express();
-const PORT = process.env.APP_PORT || 5050;
+const PORT = process.env.PORT || process.env.APP_PORT || 5050;
 
 // Middleware
 app.use(cors({
